aa1985825e832cd50bf1c72c7d0bd407889acbaf
alfredocentinaro <45145513+alfredocentinaro@users.noreply.github.com>
2022-02-14 01:43:18 +0100

    Create coppie-crecenti-decrescenti.cpp

---
+++ coppie-crescenti-decrescenti/coppie-crecenti-decrescenti.cpp
@@ -0,0 +1,39 @@
+/*
+ * Leggere un numero indefinito di interi da tastiera
+ * fino a che l'ultimo elemento inserito sia uguale
+ * al penultimo inserito. Per ogni coppia di numeri 
+ * inseriti, indicare se il secondo elemento è crescente 
+ * o decrescente rispetto al precedente
+*/ 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    int somma = 0;
+  
+    cout << "Inserisci il primo numero";
+    cin >> a;  
+    cout << "Inserisci un altro numero";
+    cin >> b; 
+  
+    while (a != b )
+    {
+      if (a < b)
+      {
+        cout << "l'ultimo numero e' decrescente rispetto al precedente "<< endl;
+      }  
+      else
+      {
+        cout << "l'ultimo numero e' crescente rispetto al precedente "<< endl;
+      } 
+
+      a = b;
+      cout << "Inserisci un altro numero";      
+      cin >> b;
+        
+ 
+    }  
+    return 0;
+}
